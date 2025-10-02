93de5b75a972cd57c604c0d4232fb9436e7cfde1
Rutika72 <103304552+Rutika72@users.noreply.github.com>
2025-10-02 23:35:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices=document.querySelectorAll(".price");
+	let total=0;
+	prices.forEach(price=>{
+		total+=parseInt(price.textContent);
+	});
+	const table=document.querySelector("table");
+	const newrow=document.querySelector("tr");
+	const totalcell=document.querySelector("th");
+
+	totalcell.setAttribute("collspan","2");
+	newrow.appendChild(totalcell);
+	table.appendChild(newrow);
+	getSumBtn=true;
   
 };
 
